12f561a72e91065a36c10a80d3e401f260836507
Dinesh121 <111.dinesh@gmail.com>
2026-04-01 02:01:28 +0000

    chore(terraform): add stacks/r3/modules/identity/user_assigned_identity/main.tf (15/26)

---
+++ stacks/r3/modules/identity/user_assigned_identity/main.tf
@@ -0,0 +1,11 @@
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_user_assigned_identity" "this" {
+  name                = var.identity_name
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+
+  tags = var.tags
+}
