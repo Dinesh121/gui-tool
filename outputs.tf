c06ac5d5808d44f0e4383345a428b567b40c9149
Dinesh121 <111.dinesh@gmail.com>
2026-04-01 02:01:21 +0000

    chore(terraform): add outputs.tf (3/26)

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,24 @@
-output "aks_id" { value = module.workload.id }
+output "r0_web_app_id" { value = module.r0_workload.id }
+output "r0_default_hostname" { value = module.r0_workload.default_hostname }
 
+output "r1_aks_id" { value = module.r1_workload.id }
+
+
+output "r2_sql_server_id" { value = module.r2_workload.id }
+output "r2_sql_fqdn" { value = module.r2_workload.fqdn }
+
+
+output "r3_identity_id" { value = module.r3_workload.id }
+output "r3_principal_id" { value = module.r3_workload.principal_id }
+
+
+output "r4_sql_server_id" { value = module.r4_workload.id }
+output "r4_sql_fqdn" { value = module.r4_workload.fqdn }
+
+
+output "r5_storage_account_id" {
+  value = module.r5_workload.id
+}
+output "r5_primary_blob_endpoint" {
+  value = module.r5_workload.primary_blob_endpoint
+}
