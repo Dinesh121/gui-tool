84904f66302d28b6d6ec40f6c0d93715dae6efb6
Dinesh121 <111.dinesh@gmail.com>
2026-04-01 01:46:15 +0000

    chore(terraform): add locals.tf (3/13)

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_tags = {
+    application         = var.application
+    environment         = var.environment
+    owner               = var.owner
+    cost_center         = var.cost_center
+    business_unit       = var.business_unit
+    data_classification = var.data_classification
+    criticality         = var.criticality
+  }
+}
