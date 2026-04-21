2b152da76adcc55cd1f216f16e16666d9569bb1a
Dinesh121 <111.dinesh@gmail.com>
2026-04-21 15:03:52 +0000

    chore(terraform): add outputs.tf (5/11)

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,15 @@
-output "r0_web_app_id" { value = module.r0_workload.id }
-output "r0_default_hostname" { value = module.r0_workload.default_hostname }
-
-output "r1_aks_id" { value = module.r1_workload.id }
-
-
-output "r2_sql_server_id" { value = module.r2_workload.id }
-output "r2_sql_fqdn" { value = module.r2_workload.fqdn }
-
-
-output "r3_identity_id" { value = module.r3_workload.id }
-output "r3_principal_id" { value = module.r3_workload.principal_id }
-
-
-output "r4_sql_server_id" { value = module.r4_workload.id }
-output "r4_sql_fqdn" { value = module.r4_workload.fqdn }
-
-
-output "r5_storage_account_id" {
-  value = module.r5_workload.id
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
 }
-output "r5_primary_blob_endpoint" {
-  value = module.r5_workload.primary_blob_endpoint
+
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.this.id
+}
+
+output "vm_principal_id" {
+  value = try(azurerm_linux_virtual_machine.this.identity[0].principal_id, null)
+}
+
+output "private_ip_address" {
+  value = azurerm_network_interface.this.private_ip_address
 }
