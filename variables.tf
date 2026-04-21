d907f7aa8a94fac89a51296f8e4fd2eec6732c80
Dinesh121 <111.dinesh@gmail.com>
2026-04-21 15:03:55 +0000

    chore(terraform): add variables.tf (10/11)

--- variables.tf
+++ variables.tf
@@ -1,129 +1,70 @@
-variable "subscription_id" { type = string }
-
-variable "r0_resource_group_name" { type = string }
-
-variable "r0_app_name" { type = string }
-
-variable "r0_service_plan_sku" { type = string }
-
-variable "r0_runtime_stack" { type = string }
-
-variable "r0_https_only" { type = bool }
-
-variable "r0_enable_system_assigned_identity" { type = bool }
-
-variable "r0_vnet_route_all_enabled" { type = bool }
-
-variable "r0_application" { type = string }
-variable "r0_environment" { type = string }
-variable "r0_owner" { type = string }
-variable "r0_cost_center" { type = string }
-variable "r0_business_unit" { type = string }
-variable "r0_data_classification" { type = string }
-variable "r0_criticality" { type = string }
-
-variable "r1_resource_group_name" { type = string }
-
-variable "r1_aks_cluster_name" { type = string }
-
-variable "r1_aks_dns_prefix" { type = string }
-
-variable "r1_aks_node_count" { type = number }
-
-variable "r1_aks_vm_size" { type = string }
-
-variable "r1_application" { type = string }
-variable "r1_environment" { type = string }
-variable "r1_owner" { type = string }
-variable "r1_cost_center" { type = string }
-variable "r1_business_unit" { type = string }
-variable "r1_data_classification" { type = string }
-variable "r1_criticality" { type = string }
-
-variable "r2_resource_group_name" { type = string }
-
-variable "r2_server_name" { type = string }
-
-variable "r2_administrator_login" { type = string }
-
-variable "r2_sql_admin_password" {
+variable "subscription_id" {
+  description = "Azure subscription ID for deployment."
   type        = string
-  sensitive   = true
-  description = "Set via TF_VAR_sql_admin_password at apply time"
 }
 
-variable "r2_minimum_tls_version" { type = string }
-
-variable "r2_public_network_access_enabled" { type = bool }
-
-variable "r2_application" { type = string }
-variable "r2_environment" { type = string }
-variable "r2_owner" { type = string }
-variable "r2_cost_center" { type = string }
-variable "r2_business_unit" { type = string }
-variable "r2_data_classification" { type = string }
-variable "r2_criticality" { type = string }
-
-variable "r3_resource_group_name" { type = string }
-
-variable "r3_identity_name" { type = string }
-
-variable "r3_application" { type = string }
-variable "r3_environment" { type = string }
-variable "r3_owner" { type = string }
-variable "r3_cost_center" { type = string }
-variable "r3_business_unit" { type = string }
-variable "r3_data_classification" { type = string }
-variable "r3_criticality" { type = string }
-
-variable "r4_resource_group_name" { type = string }
-
-variable "r4_server_name" { type = string }
-
-variable "r4_administrator_login" { type = string }
-
-variable "r4_sql_admin_password" {
+variable "resource_group_name" {
   type        = string
-  sensitive   = true
-  description = "Set via TF_VAR_sql_admin_password at apply time"
+  description = "Resource group for VM and related resources."
 }
 
-variable "r4_minimum_tls_version" { type = string }
+variable "location" {
+  type        = string
+  description = "Azure region."
+}
 
-variable "r4_public_network_access_enabled" { type = bool }
+variable "vm_name" {
+  type        = string
+  description = "Name of the virtual machine."
+}
 
-variable "r4_application" { type = string }
-variable "r4_environment" { type = string }
-variable "r4_owner" { type = string }
-variable "r4_cost_center" { type = string }
-variable "r4_business_unit" { type = string }
-variable "r4_data_classification" { type = string }
-variable "r4_criticality" { type = string }
+variable "vm_size" {
+  type        = string
+  description = "Azure VM SKU."
+}
 
-variable "r5_resource_group_name" { type = string }
+variable "admin_username" {
+  type        = string
+  description = "Local administrator username."
+}
 
-variable "r5_storage_account_name" { type = string }
+variable "admin_password" {
+  type        = string
+  description = "Required for Windows or Linux password auth. Set via TF_VAR_admin_password. Do not commit."
+  sensitive   = true
+  default     = null
+}
 
-variable "r5_account_tier" { type = string }
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key for Linux authentication when using SSH."
+  default     = null
+}
 
-variable "r5_account_replication_type" { type = string }
-
-variable "r5_account_kind" { type = string }
-
-variable "r5_access_tier" { type = string }
-
-variable "r5_min_tls_version" { type = string }
-
-variable "r5_https_traffic_only" { type = bool }
-
-variable "r5_allow_nested_items_to_be_public" { type = bool }
-
-variable "r5_public_network_access_enabled" { type = bool }
-
-variable "r5_application" { type = string }
-variable "r5_environment" { type = string }
-variable "r5_owner" { type = string }
-variable "r5_cost_center" { type = string }
-variable "r5_business_unit" { type = string }
-variable "r5_data_classification" { type = string }
-variable "r5_criticality" { type = string }
+variable "application" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "owner" {
+  type = string
+}
+variable "cost_center" {
+  type = string
+}
+variable "business_unit" {
+  type = string
+}
+variable "data_classification" {
+  type = string
+}
+variable "criticality" {
+  type = string
+}
+variable "compliance_framework" {
+  type = string
+}
+variable "network_segmentation" {
+  type = string
+}
